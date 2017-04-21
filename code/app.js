57f3edc180ec97a41fd55080e7ddda8387fa6760
Aotian Wu <155016042@qq.com>
2017-04-21 15:18:37 +0800

    update app.js

--- code/app.js
+++ code/app.js
@@ -1,17 +1,21 @@
 var express = require('express')
 
 var route = require("./routes")();
+var middlewares = require("./middlewares")();
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var logger = require('morgan');
 
-var port = 80;
+var port = 3000;
 
 
 var app = express();
 
 var compression = require('compression');
 
+var wuaotian = require("./wuaotian.json");
 
 app.use(compression());  
 
@@ -22,14 +26,26 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cookieParser("secret"));
+
+app.use(session({
+	name: wuaotian.session.name,
+	secret: wuaotian.session.secret,
+	cookie: {
+		maxAge: 60 * 60 * 1000
+	},
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(middlewares);
+
 app.use(route);
 
 // handle fallback for HTML5 history API
 app.use(require('connect-history-api-fallback')());
 
 app.use(express.static('dist'));
-
-
 
 
 
