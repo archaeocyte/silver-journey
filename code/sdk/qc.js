23bfb8e6033ab4447d0af712b2e9f5557d4aeb88
Aotian Wu <155016042@qq.com>
2017-04-28 00:33:01 +0800

    更新cosSDK

--- code/sdk/qc.js
+++ code/sdk/qc.js
@@ -1,75 +1,53 @@
-var qcSDK = require('qcloudapi-sdk'),
+var cosAPI = require('cos-nodejs-sdk-v5'),
+	cosConfig = require('cos-nodejs-sdk-v5/sdk/config'),
 	wuaotian,
+	AppId,
 	SecretId,
 	SecretKey;
 
 var parseString = require('xml2js').parseString;
-var crypto = require('crypto');
-var CryptoJS = require('crypto-js');  
+
 
 try {
 	wuaotian = require("../wuaotian.json");
+	AppId = wuaotian.qcloud.AppId;
 	SecretId = wuaotian.qcloud.SecretId;
 	SecretKey = wuaotian.qcloud.SecretKey;
 } catch(e) {
 
 }
 
-var cosAPI = new qcSDK({
-	serviceType: 'cos',
-	SecretId: wuaotian.qcloud.SecretId,
-	SecretKey: wuaotian.qcloud.SecretKey
-});
+cosConfig.setAppInfo(AppId, SecretId, SecretKey);
+
+
 
 exports.cosAPI = cosAPI;
 
 
-var getAuthorization = function(options) {
 
-	options = options || {};
-
-	// 有效时间为当前时间至五分钟后
-	var start_time = Math.floor(+new Date() / 1000);
-	var end_time = start_time + 80000;
-
-	var q_key_time = `${start_time};${end_time}`;
-	var q_sign_time = `${start_time};${end_time}`;
-
-	if (true) {
-		SecretKey = "AKIDZfbOA78asKUYBcXFrJD0a1ICvR98JM";
-		q_key_time = "1480932292;1481012292";
-		options.method = "get";
-		options.uri = "/testfile";
-		options.parameters = "";
-		options.headers
-	}
-
-	var sign_key = CryptoJS.HmacSHA1(q_key_time, SecretKey).toString();
-	console.log(sign_key);
-}
 
 if (!module.parent) {
-	var url = cosAPI.generateUrl({
-		path: '/picture/actor/1.jpg',
-		host: 'silverjourney-1253262853.cosgz.myqcloud.com'
+	cosAPI.getService({}, function(err, data) {
+		console.log(JSON.stringify(data));
 	});
-	console.log(url);
 
-	getAuthorization();
+	var path = require('path'),
+		fs = require('fs');
 
-	cosAPI.request({}, {
-		path: '/picture/actor/1.jpg?acl',
-		host: 'silverjourney-1253262853.cosgz.myqcloud.com',
-		protocol: 'http'
-	}, function(err, xml) {
-		console.log(xml);
-		parseString(xml, function(err, result) {
-			console.log(result);
+	var basePath = path.join(__dirname, "../../db_src/picture/actor/1.jpg"),
+		state = fs.statSync(basePath),
+		length = state.size;
+
+	fs.readFile(basePath, function(err, data) {
+		console.log(data.length, length, basePath);
+		cosAPI.putObject({
+			ContentLength: length,
+			Body: basePath,
+			Bucket: "silverjourney",
+			Region: "cn-south",
+			Key: "test.jpg"
+		}, function() {
+			console.log(arguments);
 		});
-	}, {
-		headers: {
-			Date: new Date()
-			//Range: "bytes=byte_range"
-		}
 	});
 }
