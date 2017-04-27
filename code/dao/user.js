a6a0eb84615e7f56cec6ba74b15740e0a5b9ef06
Aotian Wu <155016042@qq.com>
2017-04-28 01:37:30 +0800

    完成dao/user.js的查与插操作

--- code/dao/user.js
+++ code/dao/user.js
@@ -1,11 +1,39 @@
 var connection = require("./connection");
+var utils = require("../utils");
 
 module.exports = {
-	getUserByEmail: function(email, callback) {
+	getUserOR: function(options, callback) {
+		var whereStr = utils.getSqlWhereArr(options).join(" OR ");
 		var sql = `
 			select * from user
-			where email = '${email}'
+			where ${whereStr}
+		`;
+		connection.query(sql, [], callback);
+	},
+	getUserAND: function(options, callback) {
+		var whereStr = utils.getSqlWhereArr(options).join(" AND ");
+		var sql = `
+			select * from user
+			where ${whereStr}
+		`;
+		connection.query(sql, [], callback);
+	},
+	insertUser: function(options, callback) {
+		var KV = utils.getSqlKV(options);
+		var sql = `
+			insert into user (${KV.params.join(",")})
+				values (${KV.values.join(",")});
 		`;
 		connection.query(sql, [], callback);
 	}
 };
+
+
+if (!module.parent) {
+	module.exports.getUserAND({
+		username: "admin",
+		type: 99
+	}, function(err, rows) {
+		console.log(rows);
+	});
+}
