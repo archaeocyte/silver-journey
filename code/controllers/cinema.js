ac9fac11169aae84c2987a688babaa6252760bc1
Aotian Wu <155016042@qq.com>
2017-05-01 19:26:32 +0800

    优化对单个cinema的属性补齐处理逻辑

--- code/controllers/cinema.js
+++ code/controllers/cinema.js
@@ -5,22 +5,39 @@ var error_code = require("./error_code");
 var _ = require("lodash");
 var async = require("async");
 
+function fillCinemaDetail(cinema, supply, callback) {
+	var ep = supply.ep;
+
+	cinema.services = JSON.parse(cinema.service);
+	delete cinema.service;
+
+	callback(null, cinema);
+}
+
 exports.list = function list(req, res, next) {
 	var ep = new eventproxy();
 	ep.fail(next);
 
 	ep.on("fetch_cinema_list", function(cinemas) {
+		var allTask = [];
 		_.forEach(cinemas, function(cinema) {
-			cinema.services = JSON.parse(cinema.service);
-			delete cinema.service;
+			allTask.push(function(task_callback) {
+				fillCinemaDetail(cinema, {
+					ep: ep
+				}, task_callback);
+			});
 		});
-		return res.send({
-			code: error_code.SUCCESS,
-			data: {
-				cinemas: cinemas
-			},
-			msg: "SUCCESS"
+
+		async.parallelLimit(allTask, 10, function(err, cinemas) {
+			return res.send({
+				code: error_code.SUCCESS,
+				data: {
+					cinemas: cinemas
+				},
+				msg: "SUCCESS"
+			});
 		});
+
 	});
 
 	cinema_dao.getCinema(null, ep.done("fetch_cinema_list"));
