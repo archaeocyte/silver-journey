15dcf6291f34fc3abee7318e85f32fb404de180a
Aotian Wu <155016042@qq.com>
2017-05-11 02:11:51 +0800

    完成COS JS SDK的接入

--- code/src/pages/upload/photo.vue
+++ code/src/pages/upload/photo.vue
@@ -13,6 +13,7 @@
             <input type="text" name="dir_path" v-model="model_dir_path">
             <input type="submit">
         </form>
+        <div>{{percent}}</div>
     </div>
 </template>
 
@@ -23,38 +24,57 @@ module.exports = {
     data() {
         return {
             $form: null,
-            model_dir_path: ""
+            model_dir_path: "",
+            cos: null,
+            percent: ""
         };
     },
     mounted() {
         this.$form = document.getElementById("upload-photo-form");
         console.log({f: this.$form});
+        this.cos = new CosCloud({
+            appid: "1253262853",
+            bucket: "silverjourney",
+            region: "gz",
+            getAppSign: function(callback) {
+                $.ajax({
+                    url: '/api/cos/auth/get',
+                    type: "GET",
+                    success: function(result) {
+                        callback(result.data.sign);
+                    }
+                });
+            },
+            getAppSignOnce: function(callback) {
+                $.ajax({
+                    url: '/api/cos/auth/get',
+                    type: "GET",
+                    success: function(result) {
+                        callback(result.data.sign);
+                    }
+                });
+            }
+        });
+        console.log(this.cos);
     },
     methods: {
         _submitEvent(e) {
             var vm = this;
-            var fd = new FormData(this.$form);
-            var ContentLength = this.$form.children[0].files[0].size;
-            fd.append("ContentLength", ContentLength);
-            console.log(fd);
+            var file = this.$form.children[0].files[0];
+            var ContentLength = file.size;
             var dir_path = vm.model_dir_path || "";
-            var url = `
-                /api/cos/upload/photo?ContentLength=${ContentLength}&dir_path=${dir_path}
-            `;
-            $.ajax({
-                url: url,
-                type: "POST",
-                data: fd,
-                processData: false,
-                cache: false,
-                contentType: false,
-                success: function(result) {
-                    console.log(result);
-                },
-                complete: function() {
-                    
-                }
-            });
+            var pathname = file.name;
+            if (dir_path) {
+                pathname = `${dir_path}/${pathname}`;
+            }
+            this.cos.uploadFile(function(result) {
+                console.log(arguments);
+            }, function() {
+                console.log(arguments);
+            }, function(percent) {
+                vm.percent = Number((percent * 100).toFixed(2)) + "%";
+                console.log(percent);
+            }, "silverjourney", pathname, file, 0);
             e.preventDefault();
             return false;
         }
