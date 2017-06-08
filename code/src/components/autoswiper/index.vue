29a4f5256d8db8dd64856de6e19739562b79dc21
psyche-k <875710610@qq.com>
2017-06-08 18:30:20 +0800

    轮播
    增加了首页的轮播效果

---
+++ code/src/components/autoswiper/index.vue
@@ -0,0 +1,151 @@
+<template>
+
+    <swiper :options="swiperOption">
+        <swiper-slide>
+            <div>
+            <img src='../../assets/images/index/speed.png'>
+            <div  class="wordspan">
+            <ul>
+            <li>正在热映</li>
+            <li>速度与激情8</li>
+            <li>136分钟-动作/犯罪-广州121家影院上映2805场</li>
+            <li>“</li>
+            <li>硬汉豪车再纵横，依旧燃爆荷尔蒙</li></ul></div>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div>
+            <img src='../../assets/images/index/speed.png'>
+            <div class="wordspan">
+            <ul>
+            <li>正在热映</li>
+            <li>速度与激情8</li>
+            <li>136分钟-动作/犯罪-广州121家影院上映2805场</li>
+            <li>“</li>
+            <li>硬汉豪车再纵横，依旧燃爆荷尔蒙</li></ul></div>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+            <div>
+            <img src='../../assets/images/index/speed.png'>
+            <div class="wordspan">
+            <ul>
+            <li>正在热映</li>
+            <li>速度与激情8</li>
+            <li>136分钟-动作/犯罪-广州121家影院上映2805场</li>
+            <li>“</li>
+            <li>硬汉豪车再纵横，依旧燃爆荷尔蒙</li></ul></div>
+            </div>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+</template>
+
+<script>
+    import { swiper, swiperSlide } from 'vue-awesome-swiper'
+ 
+    export default {
+        components: {
+            swiper,
+            swiperSlide
+        },
+        data() {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+                    paginationClickable: true,
+                    spaceBetween: 30,
+                    centeredSlides: true,
+                    autoplay: 2500,
+                    autoplayDisableOnInteraction: false,
+                    loop:true
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    swiper {
+        text-align:center;
+        position:absulute;
+        background:#d8d8d8;
+    }
+    img {
+        position:relative;
+        width:100%;
+        height:400px;
+    }
+    ul {
+        position:absolute;
+        color:#ffffff;
+        left:14%;
+        top:25%;
+    }
+    li:nth-child(1) {
+        margin:10px;
+        font-family:PingFangSC-Regular;
+        font-size:18px;
+        letter-spacing:0;
+        line-height:42px;
+        background:rgba(250,122,3,0.98);
+        width:102px;
+        height:42px;
+        text-align:center;
+    }
+    li:nth-child(2) {
+        margin:10px;
+        font-family:PingFangSC-Medium;
+        font-size:36px;
+        letter-spacing:0;
+        line-height:48px;
+        text-shadow:0 2px 4px rgba(0,0,0,0.50);
+        text-align:left;
+    }
+    li:nth-child(3) {
+        margin:10px;
+        top:211px;
+        font-family:PingFangSC-Medium;
+        font-size:18px;
+        letter-spacing:0;
+        text-shadow:0 2px 4px rgba(0,0,0,0.50);
+        text-align:left;
+    }
+    li:nth-child(4) {
+        margin:20px;
+        font-family:PingFangSC-Medium;
+        font-size:80px;
+        color:#b96719;
+        letter-spacing:0;
+        line-height:34px;
+        width:64px;
+        height:68px;
+        text-align:center;
+        position:absolute;
+        top:150px;
+        left:-35px;
+    }
+    li:nth-child(5) {
+        margin:0px;
+        top:256px;
+        font-family:PingFangSC-Medium;
+        font-size:28px;
+        letter-spacing:0;
+        line-height:36px;
+        text-shadow:0 2px 4px rgba(0,0,0,0.50);
+        text-align:left;
+        position:absolute;
+        left:50px;
+        top:160px;
+        width:500px;
+    }
+    .swiper-pagination {
+        left-margin:auto;
+        right-margin:auto;
+        position:center;
+    }
+</style>
---
+++ code/src/components/buttonswiper/index.vue
@@ -0,0 +1,151 @@
+<template>
+    <div id="comeon">
+    <swiper :options="swiperOption">
+        <swiper-slide>
+
+                <div class="memory">
+                    <div  class="poster">
+                        <img src='../../assets/images/index/smallmemory.png'>
+                        <ul>
+                            <li>4月28日上映</li>
+                            <li>记忆大师</li>
+                            <li>导演：陈正道</li>
+                            <li>主演：黄渤，徐静蕾，段奕宏，杨子珊，许玮甯</li>
+                            <li>90分钟|悬疑、剧情、犯罪</li>
+                        </ul>
+                        </div>
+                </div>
+
+                <div class="save">
+                    <div  class="poster">
+                    <img src='../../assets/images/index/smallsave.png'>
+                    <ul>
+                    <li>4月28日上映</li>
+                    <li>春娇救志明</li>
+                    <li>导演：彭浩翔</li>
+                    <li>主演：余文乐，杨千嬅，蒋梦婕，曾国祥，秦沛</li>
+                    <li>117分钟|爱情、剧情</li></ul></div>
+                </div>
+
+                <div class="bomb">
+                    <div  class="poster">
+                    <img src='../../assets/images/index/smallbomb.png'>
+                    <ul>
+                    <li>4月28日上映</li>
+                    <li>拆弹专家</li>
+                    <li>导演：邱礼涛</li>
+                    <li>主演：刘德华，姜武，宋佳</li>
+                    <li>120分钟|动作、悬疑、犯罪</li></ul></div>
+                </div>
+        </swiper-slide>
+        <swiper-slide><img src='../../assets/images/index/hot_one.png'></swiper-slide>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+    </div>
+</template>
+
+<script>
+    import { swiper, swiperSlide } from 'vue-awesome-swiper'
+ 
+    export default {
+        components: {
+            swiper,
+            swiperSlide
+        },
+        data() {
+            return {
+                swiperOption: {
+                    nextButton: '.swiper-button-next',
+                    prevButton: '.swiper-button-prev',
+                    paginationClickable: true,
+                    spaceBetween: 30,
+                    centeredSlides: true,
+                    loop:true
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    #comeon {
+        border: 1px solid #000000;
+        width: 100%;
+        height:361px;
+    }
+
+    .poster {
+        height:361px;
+        background:rgba(0,0,0,0.50);
+        background-size: 100% 361px;
+        color:#ffffff;
+        margin:0px;
+    }
+    .poster img {
+        float:left;
+        margin:10%;
+    }
+    .poster ul {
+        float: left;
+        margin:0px;
+    }
+    .memory {
+        background-image: url("../../assets/images/index/memory.png");
+        background-size: 100% 100%;
+        display: inline-block;
+        margin:0px;
+        width:33.33%;
+        float:left;
+        height:361px;
+    }
+    .save {
+        background:rgba(0,0,0,0.50) top;
+        background-image: url("../../assets/images/index/save.png");
+        background-size: 100% 100%;
+        display: inline-block;
+        margin:0px;
+        width:33.33%;
+        float:left;
+        height:361px;
+    }
+    .bomb {
+        background:rgba(0,0,0,0.50) top;
+        background-image: url("../../assets/images/index/bomb.png");
+        background-size: 100% 100%;
+        display: inline-block;
+        margin:0px;
+        width:33.33%;
+        float:left;
+        height:361px;
+    }
+    li:nth-child(1) {
+        margin-top:67px;
+        margin-bottom:15px;
+        font-family:PingFangSC-Medium;
+        font-size:20px;
+        color:#f57905;
+        letter-spacing:0;
+        text-shadow:0 2px 4px rgba(0,0,0,0.50);
+        text-align:left;
+    }
+    li:nth-child(2) {
+        margin-bottom:15px;
+        font-family:PingFangSC-Medium;
+        font-size:22px;
+        color:#ffffff;
+        letter-spacing:0;
+        text-shadow:0 2px 4px rgba(0,0,0,0.50);
+        text-align:left;
+    }
+    li:nth-child(3), li:nth-child(4), li:nth-child(5) {
+        margin-top:0px;
+        font-family:PingFangSC-Medium;
+        font-size:14px;
+        color:#ffffff;
+        letter-spacing:0;
+        line-height:48px;
+        text-shadow:0 2px 4px rgba(0,0,0,0.50);
+        text-align:left;
+    }
+</style>
