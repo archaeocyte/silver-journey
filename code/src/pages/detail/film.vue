1402ce4d925f32cd9bacf9be89faf4ab10652026
Idan Loo <personal@siwei.lu>
2017-06-11 22:51:12 +0800

    bug fixed

--- code/src/pages/detail/film.vue
+++ code/src/pages/detail/film.vue
@@ -4,27 +4,25 @@
 </template>
 
 <script>
-
-
-module.exports = {
-    name: 'detail-film',
-    created: function() {
-    	var id = this.$route.params.id;
-    	console.log(id);
-    	this.fetchFilm(id);
-    },
-    methods: {
-    	fetchFilm(id) {
-    		$.ajax({
-    			url: "/api/film/detail/" + id,
-    			type: "GET",
-    			success: function(result) {
-    				console.log(result);
-    			}
-    		});
-    	}
+    module.exports = {
+        name: 'detail-film',
+        created: function () {
+            var id = this.$route.params.id;
+            console.log(id);
+            this.fetchFilm(id);
+        },
+        methods: {
+            fetchFilm(id) {
+                $.ajax({
+                    url: "/api/film/detail/" + id,
+                    type: "GET",
+                    success: function (result) {
+                        console.log(result);
+                    }
+                });
+            }
+        }
     }
-}
 </script>
 
 <style lang="less" scoped>
