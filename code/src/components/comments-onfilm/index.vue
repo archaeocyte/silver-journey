465560cff038646cdcf547be29ee98ba1aacc51b
Pangwenyan <pangwenyan22@qq.com>
2017-06-24 14:08:32 +0800

    添加电影详情页面的评分和评论

---
+++ code/src/components/comments-onfilm/index.vue
@@ -0,0 +1,200 @@
+<template>
+	<div id='comments-onfilm'>
+        <div id='user'>
+            <img id='portrait' src='../../assets/images/cinema/critic-portrait.png' />
+        </div>
+        <div id='right-comment'>
+            <img src='../../assets/images/film/comment-rect.png' />
+
+            <div id='others-rate-rect'>
+                <span id='critic-name'>AngelineSL</span>
+                <img id='1' class='ostars' :src='goldenURL' />
+                <img id='2' class='ostars' :src='goldenURL' />
+                <img id='3' class='ostars' :src='goldenURL' />
+                <img id='4' class='ostars' :src='goldenURL' />
+                <img id='5' class='ostars' :src='halfURL' />
+                <span id='mark'>9分</span>
+            </div>
+
+            <div id='comment-rect'>
+                <p>{{comment}}</p>
+            </div>
+            <div id='like-or-not'>
+                <img src='../../assets/images/cinema/like.png' @click="clike" />
+                <span id='like-num'>{{likes}}</span>
+                <img src='../../assets/images/cinema/dislike.png' @click="cdislike" />
+                <span id='dislike-num'>{{dislikes}}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+var clike_t = 0;
+var cdislike_t = 0;
+
+module.exports = {
+    name: 'comments-onfilm',
+    data () {
+        this.likes = 99;
+        this.dislikes = 3;
+        return {
+            likes: this.likes,
+            dislikes: this.dislikes,
+            comment: "全程高潮无尿点，开篇就是倒开烈火战车，僵尸车队在大街上出行时，真的很有zombie的感觉，“五车分尸”的桥段也很赞。CT扮演的大反派，颜值高气场强，但是手下的武力值太弱；JS洗白得也太容易了，他老妈的出场倒是挺出彩的；片中两次提到Brian，小伤感；小baby萌萌哒，每个镜头都敲可爱。最让我感动的镜头是潜艇爆炸时，所有人飞车前来挡在Dom身前，真的是家人才有的感情啊。最后吐槽一下基本上没什么感觉的3D特效～",
+            grayURL: require('../../assets/images/cinema/halfStar.png'),
+            goldenURL: require('../../assets/images/cinema/goldenStar.png'),
+            halfURL: require('../../assets/images/cinema/halfStar.png')
+        };
+        
+    },
+    method: {
+        clike: function () {
+            // click LIKE, number++
+            if (clike_t == 0) {
+                this.likes += 1
+                clike_t = 1;
+            } 
+            // cancel click, number--
+            else if (clike_t == 1) {
+                this.likes -= 1
+                clike_t = 0;
+            }
+                alert("?")
+
+        },
+        cdislike: function () {
+            // click DISLIKE, number++
+            if (cdislike_t == 0) {
+                this.dislikes += 1
+                cdislike_t = 1;
+            } 
+            // cancel click, number--
+            else if (cdislike_t == 1) {
+                this.dislikes -= 1
+                cdislike_t = 0;
+            }
+        }
+    },
+    components: {
+        
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+#comments-onfilm {
+    margin-top: 60px;
+    height: 237px;
+}
+
+#user {
+    width: 87px;
+    height: 237px;
+    float: left;
+}
+
+#portrait {
+    width: 60px;
+    height: 60px;
+    margin-top: 19px;
+}
+
+#portrait:hover {
+    cursor: pointer;
+}
+
+#right-comment {
+    /*margin: -159px 0px 0px 90px;*/
+    float: right;
+    width: 890px;
+}
+
+#others-rate-rect {
+    margin: -219px 0px 0px 25px;
+    span {
+        font-family:PingFangSC-Regular;
+        font-size:20px;
+        letter-spacing:0.38px;
+        line-height:32px;
+        text-align:left;
+        position: relative;
+        top: -2px;
+    }
+}
+
+.ostars {
+    width: 20px;
+    height: 20px;
+    margin-right: 15px;
+    cursor: pointer;
+}
+
+
+#critic-name {
+    color:#666666;
+    margin-right: 20px;
+    width: auto;
+    cursor: pointer;
+}
+
+#mark {
+    color:#f57905;
+}
+
+
+#comment-rect {
+    margin: 25px 0px 0px 34px;
+    p {
+        font-family:PingFangSC-Regular;
+        font-size:16px;
+        color:#999999;
+        letter-spacing:0.3px;
+        text-align:left;
+    }
+}
+
+p {
+    width: 822px;
+    font-family:PingFangSC-Regular;
+    font-size:16px;
+    color:#999999;
+    letter-spacing:0.3px;
+    text-align:left;
+    margin-top: -10px;
+}
+
+#like-or-not {
+    margin-left: 754px;
+    margin-top: 30px;
+    img {
+        cursor: pointer;
+    }
+    img:hover {
+        position: relative;
+        top: -3px;
+    }
+}
+
+span {
+    font-family:PingFangSC-Regular;
+    font-size:18px;
+    letter-spacing:0.34px;
+    text-align:left;
+    margin-left: 10px;
+    position: relative;
+    top: -3px;
+}
+
+#like-num {
+    color:#f57905;
+    margin-right: 10px;
+}
+
+#dislike-num {
+    color:#999999;
+}
+
+</style>
--- code/src/pages/detail/film.vue
+++ code/src/pages/detail/film.vue
@@ -6,8 +6,7 @@
             <span id='right'>电影详情</span>
         </div>
         <div id='poster'>
-            <img :src='pic' id='speed_pic'/>
-            <img :src='pic_two' id='xiyou_pic'/>
+            <img :src='pic' />
             <div id='wrapper'>
 	            <div id='hotting'>
 	            正在热映
@@ -15,51 +14,57 @@
 	            <div id='name'>
 	            {{filmName}}
 	            </div>
-	            <div id='duration'>
-	                <span>{{time}}分钟</span>&nbsp-
-	                <span>{{type_one}}</span>/
-	                <span>{{type_two}}</span>&nbsp-
-	                <span>{{number}}</span>
-	            </div>
-	            <div id='slogan'>
-	                <span id='mark'>"</span>
-	                <span id='ad'>{{ad}}</span>
-	            </div>
-	            <div id='select' @mouseenter='strong' @mouseleave='weak'>
-	            选座购票
-	            </div>
 	        </div>
         </div>
-        <div id='desc'>
-            <div id='left'>
-                <img :src='poster_1' id='speed_img'/>
-                <img :src='poster_2' id='xiyou_img'/> 
+
+	<div class='rate-and-comment'>
+        <div id='blank78h'></div>
+        <div class='nav-bar-wrapper'>
+            <div id='rate-title-bar'>
+                <img class='title-img' src='../../assets/images/cinema/rating.png' />
+                <span class='title'>我要评分</span>
             </div>
-            <div id='right'>
-                <div id='score'>
-                评分： 9.2
+            <div id='comment-area'>
+                <user></user>
+                <div id='right-comment'>
+                    <img src='../../assets/images/cinema/rectangle.png' />
+                    <rate-rect id='users'></rate-rect>
+                    <img id='line' src='../../assets/images/cinema/line.png' />
+                    <div id='comment-rect'>
+                        <textarea id='comment-content' v-model="comments" @click="getFocus" @blur="loseFocus" @input="inputting">{{comments}}</textarea>
+                        <span id="hint">写下您的评论…</span>
+                    </div>
+                    <div id='word-limit'>
+                        <span id='word-count'>{{comments.length}}/200</span>
+                    </div>
                 </div>
-                <div id='director'>
-                导演：{{director}}
-                </div>
-                <div id='actor'>
-                    <span id='zhuyan'>主演：</span>
-                    <Actors></Actors>
-                </div>
-                <div id='type'>
-	                <span>{{type_one}}  {{type_two}}  {{type_three}}</span> |
-	                <span>{{language}}</span> |
-	                <span>{{time}}分钟</span> |
-	                <span>{{showtime}}</span>
+                <div id='comment-button'>
+                    <span id='btn-content'>发表</span>
                 </div>
             </div>
+
+            <div id='blank60h'></div>
+            <div id='comment-title-bar'>
+                <img class='title-img' src='../../assets/images/cinema/comments.png' />
+                <span class='title'>影评</span>
+            </div>
+
+            <comments-onfilm></comments-onfilm>
+            <comments-onfilm></comments-onfilm>
+            <comments-onfilm></comments-onfilm>
+            <comments-onfilm></comments-onfilm>
+            <comments-onfilm></comments-onfilm>
+
+            <div id='blank233h'></div>
         </div>
+        </div>
+
+        
+
     </div>
 </template>
 
 <script>
-
-
 module.exports = {
     name: 'detail-film',
     created: function() {
@@ -67,25 +72,12 @@ module.exports = {
     	console.log(id);
     	this.fetchFilm(id);
     },
-    components: {
-        'Actors': require("../../components/buttonswiper/film.vue")
-    },
     data() {
+        this.comments = "";
     	return {
+    		comments: this.comments,
             pic:  require('../../assets/images/index/speed.png'),
-            pic_two: require('../../assets/images/film/xiyou.png'),
-            poster_1: require('../../assets/images/index/hot_one.png'),
-            poster_2: require('../../assets/images/index/hot_two.png'),
-            filmName: '',
-            time: '',
-            type_one: '',
-            type_two: '',
-            type_three: '',
-            number: '',
-            ad: '',
-            director: '',
-            language: '',
-            showtime: '2017年4月14日上映'
+            filmName: ''
     	};
     },
     methods: {
@@ -96,47 +88,35 @@ module.exports = {
     			type: "GET",
     			success: function(result) {
     				console.log(result);
+    				console.log(result.data.film.name_cn);
     				this.filmName = result.data.film.name_cn;
-    				this.time = result.data.film.duration;
-    				this.type_one = result.data.film.types[0];
-    				if (result.data.film.types[3]) {
-    				    this.type_two = result.data.film.types[3];
-    				} else {
-    					this.type_two = result.data.film.types[1];
-    				}
-    				this.type_three = result.data.film.types[2];
-    				this.number = "广州121家影院上映2805场";
-    				this.ad = result.data.film.slogan;
-    				this.director = result.data.film.director.name;
-    				this.language = result.data.film.languages[0].name;
-    				if (id == 1) {
-			    		var a = document.getElementById('speed_pic');
-			    		var b = document.getElementById('xiyou_pic');
-			    		a.style.opacity = '0';
-			    		b.style.opacity = '100';
-			    		var c = document.getElementById('speed_img');
-			    		var d = document.getElementById('xiyou_img');
-			    		c.style.opacity = '0';
-			    		d.style.opacity = '100';
-			    	} else if (id == 2) {
-			    		var a = document.getElementById('speed_pic');
-			    		var b = document.getElementById('xiyou_pic');
-			    		b.style.opacity = '0';
-			    		a.style.opacity = '100';
-			    		var c = document.getElementById('speed_img');
-			    		var d = document.getElementById('xiyou_img');
-			    		d.style.opacity = '0';
-			    		c.style.opacity = '100';
-			    	}
     			}.bind(self)
     		});
     	},
-    	strong: function(event) {
-            event.target.style.opacity = '100';
-    	},
-    	weak: function(event) {
-            event.target.style.opacity = '0.8';
-    	}
+
+    	getFocus: function () {
+            document.getElementById("hint").style.display = "none"
+            
+        },
+
+        loseFocus: function() {
+            if (this.comments == "")
+                document.getElementById("hint").style.display = "inline"
+        },
+
+        inputting: function () {
+        	if (this.comments.length > 200) {
+            	document.getElementById('word-count').style.color = '#f57905'
+            } else {
+            	document.getElementById('word-count').style.color = '#999999'
+            }
+        }
+    },
+    components: {
+    	"login-window": require("../../components/login-window/index.vue"),
+    	"rate-rect": require("../../components/rate-rect/index.vue"),
+        "user": require("../../components/user/index.vue"),
+        "comments-onfilm": require("../../components/comments-onfilm/index.vue")
     }
 }
 </script>
@@ -174,7 +154,6 @@ module.exports = {
 		margin-left: 9px;
 	}
 }
-
 #poster {
 	width: 100%;
 	height: 400px;
@@ -184,7 +163,6 @@ module.exports = {
 		width: 100%;
 		height: 400px;
 		position: absolute;
-		opacity: 0;
 	}
 	#wrapper {
 		width: 980px;
@@ -205,7 +183,7 @@ module.exports = {
 			top: 80px;
 		}
 		#name {
-			width: 350px;
+			width: 202px;
 			height: 48px;
 			font-family:PingFangSC-Medium;
 			font-size:36px;
@@ -217,122 +195,125 @@ module.exports = {
 			position: absolute;
 			top: 143px;
 		}
-		#duration {
-			width: 410px;
-			height: 25px;
-			position: absolute;
-			top: 210px;
-			font-family:PingFangSC-Medium;
-			font-size:18px;
-			color:#ffffff;
-			letter-spacing:0;
-			text-shadow:0 2px 4px rgba(0,0,0,0.50);
-			text-align:left;
-		}
-		#slogan {
-			width: 500px;
-			height: 36px;
-			position: absolute;
-			top: 255px;
-			#mark {
-				width: 34px;
-                height: 36px;
-                font-family:PingFangSC-Medium;
-				font-size:56px;
-				color:#b96719;
-				letter-spacing:0;
-				line-height:64px;
-				text-align:left;
-				margin-right: 26px;
-			}
-			#ad {
-				height: 36px;
-				width: 420px;
-				font-family:PingFangSC-Medium;
-				font-size:28px;
-				color:#ffffff;
-				letter-spacing:0;
-				line-height:36px;
-				text-shadow:0 2px 4px rgba(0,0,0,0.50);
-				text-align:left;
-				position: relative;
-				top: -15px;
-			}
-		}
-		#select {
-			width: 268px;
-			height: 81px;
-			line-height: 81px;
-			position: absolute;
-			top: 318px;
-			left: 670px;
-			background:rgba(245,120,5,0.80);
-			opacity: 0.8;
-			font-family:PingFangSC-Medium;
-			font-size:28px;
-			color:#ffffff;
-			letter-spacing:0;
-			text-align:center;
-			cursor: pointer;
-		}
 	}
 }
-#desc {
-	width: 980px;
-	height: 320px;
-	margin: 0 auto;
-	margin-bottom: 70px;
-	#left {	
-		width: 220px;
-        height: 320px;
-        float: left;
-		img {
-			width: 220px;
-			height: 290px;
-	        position: absolute;
-	        opacity: 0;
-		}
-	}
-	#right {
-        width: 700px;
-        height: 320px;
-        margin-left: 40px;
-        float: left;
-        #score {
-        	font-family:PingFangSC-Medium;
-			font-size:35px;
-			color:rgba(245,120,5,0.80);
-			letter-spacing:0;
-			text-align:left;
-			margin-top: 15px;
-        }
-        #director {
-            font-family:PingFangSC-Medium;
-			font-size:18px;
-			color:#666666;
-			letter-spacing:0;
-			text-align:left;
-			margin-top: 10px;
-        }
-        #actor {
-        	height: 135px;
-        	margin-top: 15px;
-        	#zhuyan {
-        		font-family:PingFangSC-Medium;
-				font-size:18px;
-				color:#666666;
-				letter-spacing:0;
-				text-align:left;
-        	}
-        }
-        #type {
-        	font-family:PingFangSC-Medium;
-			font-size:18px;
-			color:#666666;
-			letter-spacing:0;
-			text-align:left;
-        }
-	}
 
+
+/* rate-and-comment */
+.nav-bar-wrapper {
+    width: 980px;
+    margin: 0 auto;
+    height: 100%;
+}
+
+.rate-and-comment {	
+    background:#f5f5f5;
+    height: 100%;
+    margin-top: 2px;
+}
+
+#blank78h {
+    height: 78px;
+}
+
+.title-img {
+    position: relative;
+    top: 5px;
+    margin-right: 15px;
+}
+
+.title {
+    font-family:PingFangSC-Regular;
+    font-size:20px;
+    color:#333333;
+    letter-spacing:0.37px;
+    text-align:left;
+}
+
+#comment-area {
+    margin-top: 53px;
+    height: 250px;
+}
+
+#right-comment {
+    margin: -250px 0px 0px 93px;
+    width:890px;
+    height:250px;
+}
+
+#users {
+    margin: -194px 0px 0px 35px;
+}
+
+#comment-button {
+	background:#f57905;
+    margin: -40px 0px 0px 900px;
+    height: 40px;
+    cursor: pointer;
+}
+
+#comment-button:hover {
+    background-color: #C86203;
+}
+
+#btn-content {
+    font-family:PingFangSC-Regular;
+    font-size:16px;
+    color:#ffffff;
+    letter-spacing:0.3px;
+    text-align:left;
+    position: relative;
+    top: 10px;
+    left: 24px;
+}
+
+#line {
+    margin: 0px 0px 0px 35px;
+    width:310px;
+}
+
+#comment-rect {
+    margin: 14px 0px 0px 35px;
+    span {
+        font-family:PingFangSC-Regular;
+        font-size:16px;
+        color:#999999;
+        letter-spacing:0.3px;
+        text-align:left;
+        margin-left: -827px;
+        position: relative;
+        top: -75px;
+    }
+}
+
+#comment-content {
+    border:1px solid white;
+    height: 88px;
+    width: 822px;
+    font-family:PingFangSC-Regular;
+    font-size:16px;
+    color:#999999;
+    letter-spacing:0.3px;
+    text-align:left;
+}
+
+#word-limit {
+    margin-left: 826px;
+}
+
+#word-count {
+    font-family:PingFangSCRegular;
+    font-size:16px;
+    color:#999999;
+    letter-spacing:0.3px;
+    text-align:left;
+}
+
+#blank60h {
+    height: 60px;
+}
+
+#blank233h {
+    height: 233px;
 }
 </style>
